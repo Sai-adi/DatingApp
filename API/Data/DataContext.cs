d55311572798766b71e3016b4b1e3f37f21936d6
Sai-adi <masterful.adi@gmail.com>
2025-05-20 18:52:59 +0530

    Initial Commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using System;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace API.Data;
+
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
