b4c3bfd3f7874dd091023e34aefe6e5b81fafb58
Sai-adi <masterful.adi@gmail.com>
2025-05-26 20:51:14 +0530

    modifications

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,25 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Component, inject, OnInit } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent implements OnInit {
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
   
-//   http = inject(HttpClient);
-//   title = 'Dating App';
-//   users: any;
+  http = inject(HttpClient);
+  title = 'Dating App';
+  users: any;
 
-//   ngOnInit(): void {
-//     this.http.get('https://localhost:5001/api/users').subscribe({
-//       next: response => this.users=response,
-//       error: error => console.log(error),
-//       complete: () => console.log('Request has completed')
-//     })
-//   }
-// }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users=response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+}
